b7c0b451b1d0ba621336a831ac3ead954ae5788b
shikha0 <47059718+shikha0@users.noreply.github.com>
2025-08-19 16:01:55 +0530

    Update variables.tf

---
+++ child-module/variables.tf
@@ -0,0 +1,15 @@
+variable "cidr" {}
+variable "tenancy" {}
+variable "public_subnet" {}
+
+variable "private_subnet" {}
+
+variable "azs_private" {}
+variable "azs_public" {}
+variable "instance_type" {}
+variable "key_pair-name" {}
+variable "tags" {}
+variable "tags2" {}
+variable "ports" {}
+variable "private_key_path" {}
+variable "instance_type_kubernetes" {}
